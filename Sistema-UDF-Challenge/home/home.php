7ff713bafa040aaf8cdfc2ec298c6da8b898b864
RobsonR07 <44735328+RobsonR07@users.noreply.github.com>
2018-12-03 21:31:10 -0200

    Sistema PHP - Fim 28-11

---
+++ Sistema-UDF-Challenge/home/home.php
@@ -0,0 +1,66 @@
+
+
+    <!-- Banner -->
+    <img src="./img/banner-final.jpg" class="img-fluid px-0" alt="Responsive image">
+    <br/><br/>
+    <div id="corpo" class="container-fluid">   
+    <!-- Cards -->
+    <div class="card-deck">
+        <div id="cards" class="card card-signin">
+            <img class="card-img-top" src="./img/fotoudf.png" alt="Img da Instituição"/>
+            <div class="card-body">
+            <h5 class="card-title">O UDF Challenge</h5>
+            <p class="card-text">O UDF Challenge é um projeto educacional, que visa a execursão de atividades extracurriculares para alunos de Tecnologia da Informação (TI) da UDF.<br/> Utilizado pelo professores para desafiar alunos e ex-alunos da instituição. </p>
+            </div>
+        </div>
+        <div id="cards" class="card card-signin">
+            <img class="card-img-top" src="./img/img-comp.jpg" alt="Desempenho"/>
+            <div class="card-body">
+            <h5 id="card-title" class="card-title">Ganho de Desempenho</h5>
+            <p class="card-text">A competição pode ser uma ferramenta muito útil no ambiente de ensino, desde que executada de maneira cooperativa. A UDF Challenge foi desenvolvido com o intuito de aumentar a produtividade dos alunos da instituição, tendo esse conceito em mente.</p>
+            </div>
+        </div>
+        <div id="cards" class="card card-signin">
+            <img class="card-img-top" src="./img/diferencial.png" alt="Diferenciais do Sistema"/>
+            <div class="card-body">
+            <h5 class="card-title">Diferenciais</h5>
+            <p class="card-text">Sendo uma ferramenta de estudo extra (extensora), a UDF Challenge digitaliza tarefas e deveres de casa para facilitar suas resoluções enquanto promove uma maior interatividade e comunicação entre os membros de uma comunidade.</p>
+            </div>
+        </div>
+    </div>
+</div>
+<style>
+
+div#corpo{
+    margin-top: 5%;
+    margin-bottom: 5%;
+}
+
+/*div#cards{
+    box-shadow: 10px 10px rgba(0,0,0,0.7); 
+}*/
+
+.no-padding {
+  padding-left: 0;
+  padding-right: 0;
+}
+
+.card-signin {
+  border: 0;
+  border-radius: 2rem;
+  box-shadow: 0 1rem 1rem 0 rgba(0, 0, 0, 0.2);
+}
+
+.card-signin .card-title {
+  margin-bottom: 2rem;
+  font-weight: 300;
+  font-size: 1.5rem;
+}
+
+.card-signin .card-body {
+  padding: 2rem;
+}
+
+
+
+</style>
